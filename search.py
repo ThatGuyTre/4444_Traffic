3e6e1d445f83ef483683147a95fa74f806b3d64a
Michael <93162862+michaelclesi@users.noreply.github.com>
2023-11-27 22:33:41 -0600

    fix

--- search.py
+++ search.py
@@ -13,17 +13,18 @@ def heuristic(node1, node2, nodes):
     - float: The estimated cost between node1 and node2.
     """
     
-    x1, y1 = nodes[node1][3], nodes[node1][4]  # Adjusted indices
-    x2, y2 = nodes[node2][3], nodes[node2][4]  # Adjusted indices
+    x1, y1 = nodes[node1][3], nodes[node1][4]  
+    x2, y2 = nodes[node2][3], nodes[node2][4]  
     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 
 class RoadNetworkProblem(Problem):
 
-    def __init__(self, initial, goal, graph, nodes):
+    def __init__(self, initial, goal, graph, nodes, edges):
         super().__init__(initial, goal)
         self.graph = graph
-        self.nodes = nodes  # Store the nodes dictionary
+        self.nodes = nodes  
+        self.edges = edges
 
     def actions(self, state):
         return list(self.graph.successors(state))
@@ -32,7 +33,25 @@ class RoadNetworkProblem(Problem):
         return action
     
     def path_cost(self, c, state1, action, state2):
-        return c + 1
+
+        node_color = self.nodes[state1][-1]
+        node_delay = self.nodes[state1][-2]
+
+
+        edge_key = (state1, state2)
+
+        if edge_key in self.edges:
+            edge_length = self.edges[edge_key][-1]  
+        else:
+            edge_length = 0
+
+        print(f"Edge from {state1} to {state2}: Length = {edge_length}")
+        
+        if node_color == 'red':
+            return c + node_delay + edge_length
+        else:
+            return c + 1 + edge_length
+
     
     def h(self, node):
-        return heuristic(node.state, self.goal, self.nodes)  # Use the nodes dictionary
+        return heuristic(node.state, self.goal, self.nodes)  
